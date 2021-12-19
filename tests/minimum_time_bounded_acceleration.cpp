dad966e19b15fc00c84391864c96e4ba848acda3
Rojas Rafael <rafael.rojas@unibz.it>
2021-12-19 14:46:17 +0100

    test:min_acc print cpu time

--- tests/minimum_time_bounded_acceleration.cpp
+++ tests/minimum_time_bounded_acceleration.cpp
@@ -1,4 +1,5 @@
 #include <chrono> // for high_resolution_clock
+#include <ctime>
 #include <gsplines/Basis/BasisLegendre.hpp>
 #include <gsplines/Optimization/ipopt_solver.hpp>
 #include <iostream>
@@ -6,9 +7,11 @@
 
 using namespace opstop;
 int main() {
-  std::size_t number_of_wp = 3;
+  std::size_t number_of_wp = 10;
   std::size_t codom_dim = 7;
   std::size_t nglp = 10;
+
+  double acc_max = 10.0;
 
   double exec_time = (double)number_of_wp - 1.0;
   double ti = 0.5 * exec_time;
@@ -66,11 +69,13 @@ int main() {
   assert(acc_value < 1.0e-9);
 
   double mean_time = 0.0;
-  std::size_t number_of_tests = 200;
+  std::size_t number_of_tests = 1;
+  double time_elapsed_ms = 0;
   for (std::size_t _ = 0; _ < number_of_tests; _++) {
+    std::clock_t t_cpu_1 = std::clock();
     auto t1 = std::chrono::high_resolution_clock::now();
     gsplines::functions::FunctionExpression _diffeo =
-        minimum_time_bouded_acceleration(trj, ti, 10);
+        minimum_time_bouded_acceleration(trj, ti, acc_max);
     gsplines::functions::FunctionExpression _diffeo_diff_1 = _diffeo.derivate();
     gsplines::functions::FunctionExpression _diffeo_diff_2 =
         _diffeo_diff_1.derivate();
@@ -85,11 +90,14 @@ int main() {
     Eigen::MatrixXd _diffeo_diff_1_hist = diffeo_diff_1(time_span);
     Eigen::MatrixXd _diffeo_diff_2_hist = diffeo_diff_2(time_span);
     auto t2 = std::chrono::high_resolution_clock::now();
+    std::clock_t t_cpu_2 = std::clock();
+    time_elapsed_ms = 1000.0 * (t_cpu_2 - t_cpu_1) / CLOCKS_PER_SEC;
     std::chrono::duration<double, std::milli> ms_double = t2 - t1;
     mean_time += ms_double.count();
   }
   mean_time /= number_of_tests;
   assert(mean_time < 100.0);
   printf("Mean optimization time is %+14.7e ms\n", mean_time);
+  printf("Mean optimization time is %+14.7e ms CPU\n", time_elapsed_ms);
   return 0;
 }
