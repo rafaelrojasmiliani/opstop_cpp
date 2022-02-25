ff6b220394f6db475416a500d34f952a69720bde
Rafael A. Rojas <rafaelrojasmiliani@gmail.com>
2022-02-25 18:03:48 +0100

    fix torque constraints

--- src/torque_constraint.cpp
+++ src/torque_constraint.cpp
@@ -22,6 +22,12 @@ TorqueConstraint::TorqueConstraint(
 }
 
 Eigen::VectorXd TorqueConstraint::GetValues() const {
+  Eigen::VectorXd x =
+      GetVariables()->GetComponent("parametrization_variables")->GetValues();
+  helper_.set_diffeo(x(0), x(1));
+  helper_.compute_s_and_its_derivatives_wrt_tau();
+  helper_.compute_q_and_its_derivatives_wrt_s();
+  helper_.compute_q_and_its_derivatives_wrt_t();
   for (std::size_t uici = 0; uici < helper_.nglp_; uici++) {
     pinocchio::rnea(model_, data_, helper_.q_val_buff_.row(uici).transpose(),
                     helper_.q_diff_1_wrt_t_buff_.row(uici).transpose(),
@@ -37,18 +43,43 @@ ifopt::Component::VecBound TorqueConstraint::GetBounds() const {
 
 void TorqueConstraint::FillJacobianBlock(std::string _set_name,
                                          Jacobian &_jac_block) const {
+  Eigen::VectorXd x =
+      GetVariables()->GetComponent("parametrization_variables")->GetValues();
+  std::size_t codom_dim = helper_.position_->get_codom_dim();
+  Eigen::MatrixXd result(helper_.nglp_ * codom_dim, 2);
+  helper_.set_diffeo(x(0), x(1));
+  helper_.compute_s_and_its_derivatives_wrt_tau();
+  helper_.compute_q_and_its_derivatives_wrt_s();
+  helper_.compute_q_and_its_derivatives_wrt_t();
+
+  helper_.compute_s_and_its_derivatives_wrt_Ts_sf();
+  helper_.compute_q_partial_diff_wrt_Ts();
+  helper_.compute_q_partial_diff_wrt_sf();
+  helper_.compute_q_diff_1_wrt_t_partial_diff_wrt_Ts();
+  helper_.compute_q_diff_1_wrt_t_partial_diff_wrt_sf();
+  helper_.compute_q_diff_2_wrt_t_partial_diff_wrt_Ts();
+  helper_.compute_q_diff_2_wrt_t_partial_diff_wrt_sf();
+
   for (std::size_t uici = 0; uici < helper_.nglp_; uici++) {
     pinocchio::computeRNEADerivatives(
         model_, data_, helper_.q_val_buff_.row(uici).transpose(),
         helper_.q_diff_1_wrt_t_buff_.row(uici).transpose(),
         helper_.q_diff_2_wrt_t_buff_.row(uici).transpose());
-    Eigen::MatrixXd result =
+    result.col(0).segment(uici * codom_dim, codom_dim) =
+        data_.dtau_dq * helper_.q_diff_wrt_Ts_buff_.row(uici).transpose() +
+        data_.dtau_dv *
+            helper_.q_diff_1_wrt_t_diff_wrt_Ts_buff_.row(uici).transpose() +
+        data_.M.selfadjointView<Eigen::Upper>() *
+            helper_.q_diff_2_wrt_t_diff_wrt_Ts_buff_.row(uici).transpose();
+
+    result.col(1).segment(uici * codom_dim, codom_dim) =
         data_.dtau_dq * helper_.q_diff_wrt_sf_buff_.row(uici).transpose() +
         data_.dtau_dv *
             helper_.q_diff_1_wrt_t_diff_wrt_sf_buff_.row(uici).transpose() +
-        data_.M *
+        data_.M.selfadjointView<Eigen::Upper>() *
             helper_.q_diff_2_wrt_t_diff_wrt_sf_buff_.row(uici).transpose();
   }
+  //_jac_block = result;
 }
 
 Eigen::VectorXd TorqueConstraint::__GetValues(Eigen::Vector2d &_x) const {
