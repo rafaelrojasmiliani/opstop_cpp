683936682ee1e9b26b60fc95d36d6cb4108d93f7
Rafael A. Rojas <rafaelrojasmiliani@gmail.com>
2022-02-25 18:07:00 +0100

    test jerk constraints

--- src/jerk_l2_constraints.cpp
+++ src/jerk_l2_constraints.cpp
@@ -27,7 +27,7 @@ Eigen::VectorXd JerkL2Constraints::GetValues() const {
           .array()
           .sum();
 
-  value_buff_(0) = result;
+  value_buff_(0) = result * 0.5;
   return value_buff_;
 }
 
@@ -38,7 +38,7 @@ ifopt::Component::VecBound JerkL2Constraints::GetBounds() const {
 void JerkL2Constraints::FillJacobianBlock(std::string _set_name,
                                           Jacobian &_jac_block) const {
 
-  Eigen::VectorXd x =
+  const Eigen::VectorXd x =
       GetVariables()->GetComponent("parametrization_variables")->GetValues();
   helper_.set_diffeo(x(0), x(1));
   helper_.compute_s_and_its_derivatives_wrt_tau();
@@ -48,15 +48,25 @@ void JerkL2Constraints::FillJacobianBlock(std::string _set_name,
   helper_.compute_q_diff_3_wrt_t_partial_diff_wrt_Ts();
   helper_.compute_q_diff_3_wrt_t_partial_diff_wrt_sf();
 
-  Eigen::Map<const Eigen::VectorXd> col_0(
-      helper_.q_diff_3_wrt_t_diff_wrt_Ts_buff_.data(), GetRows());
-  Eigen::Map<const Eigen::VectorXd> col_1(
-      helper_.q_diff_3_wrt_t_diff_wrt_sf_buff_.data(), GetRows());
+  _jac_block.coeffRef(0, 0) =
+      ((helper_.q_diff_3_wrt_t_diff_wrt_Ts_buff_.array() *
+        helper_.q_diff_3_wrt_t_buff_.array())
+           .rowwise()
+           .sum()
+           .array() *
+       glw_.array())
+          .array()
+          .sum();
 
-  for (std::size_t uici = 0; uici < GetRows(); uici++) {
-    _jac_block.coeffRef(uici, 0) = col_0(uici);
-    _jac_block.coeffRef(uici, 1) = col_1(uici);
-  }
+  _jac_block.coeffRef(0, 1) =
+      ((helper_.q_diff_3_wrt_t_diff_wrt_sf_buff_.array() *
+        helper_.q_diff_3_wrt_t_buff_.array())
+           .rowwise()
+           .sum()
+           .array() *
+       glw_.array())
+          .array()
+          .sum();
 }
 
 Eigen::VectorXd JerkL2Constraints::__GetValues(Eigen::Vector2d &_x) const {
@@ -86,11 +96,6 @@ Eigen::MatrixXd JerkL2Constraints::__GetJacobian(Eigen::Vector2d &_x) const {
   helper_.compute_q_diff_3_wrt_t_partial_diff_wrt_sf();
 
   Eigen::MatrixXd result(GetRows(), 2);
-
-  result.col(0) = Eigen::Map<const Eigen::VectorXd>(
-      helper_.q_diff_3_wrt_t_diff_wrt_Ts_buff_.data(), GetRows());
-  result.col(1) = Eigen::Map<const Eigen::VectorXd>(
-      helper_.q_diff_3_wrt_t_diff_wrt_sf_buff_.data(), GetRows());
 
   result(0, 0) = ((helper_.q_diff_3_wrt_t_diff_wrt_Ts_buff_.array() *
                    helper_.q_diff_3_wrt_t_buff_.array())
